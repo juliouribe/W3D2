6a2cf052614d281a3aa07ebeac6893443ce05ae7
Julio Uribe <uribejulioc@gmail.com>
2023-06-06 13:57:03 -0700

    Add Card class

---
+++ card.rb
@@ -0,0 +1,23 @@
+class Card
+  def initialize(value)
+    @value = value
+    @faceup = false
+  end
+
+  def hide
+    @faceup = false
+  end
+
+  def reveal
+    @faceup = true
+  end
+
+  def to_s
+    showing = @faceup ? 'face-up' : 'face-down'
+    "#{@value} is #{showing}"
+  end
+
+  def ==(element)
+    @value == element
+  end
+end
